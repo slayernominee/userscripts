84b1e8a1f91537e7b6f278bd01457628367e6764
slayernominee <slayernominee_git@proton.me>
2023-05-24 11:45:00 +0200

    google advanced search fix

--- google_advanced_search_button.user.js
+++ google_advanced_search_button.user.js
@@ -4,20 +4,24 @@
 // @match       *://www.google.*/*
 // @grant       none
 // @license     MIT
-// @version     1.1
+// @homepageURL https://github.com/slayernominee/userscripts
+// @version     1.2
 // @author      slayernominee
 // @description add an button for the google advanced search in the google home search page instead of the random search button
 // ==/UserScript==
 
-random_search_button = document.getElementsByClassName("RNmpXc")[1];
+random_search_button_container = document.getElementsByTagName("center")[1];
+random_search_button = random_search_button_container.childNodes[3];
 
 random_search_button.value = "Advanced Search";
 random_search_button.setAttribute("aria-label", "Advanced Search");
-random_search_button.attributes.removeNamedItem("jsaction");
-random_search_button.attributes.removeNamedItem("data-ved");
-random_search_button.attributes.removeNamedItem("name");
-random_search_button.addEventListener('click',function (evt) {
+random_search_button.id = null;
+random_search_button.setAttribute("jsaction", "null");
+random_search_button.setAttribute("data-ved", "null");
+random_search_button.setAttribute("name", "null");
+
+random_search_button.addEventListener('click', function (evt) {
     evt.preventDefault();
 
     window.location.href = "https://www.google.com/advanced_search";
-  });
+});
